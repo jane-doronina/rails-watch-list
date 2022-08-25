bee88df7c9426247a55849e9327ec6fa450d5619
Zhanna Doronina <doronina.jane@mail.ru>
2022-08-25 21:51:19 +0100

    added some styling (not finished)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :lists, only: [:index, :new, :create, :show] do
+    resources :bookmarks, only: [:new, :create ]
+  end
+  resources :bookmarks, only: [ :destroy ]
 end
