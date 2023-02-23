dd2605c38b96583bc0d6ccaf10794e6ee7be9b1d
Zhanna Doronina <doronina.jane@mail.ru>
2023-02-23 11:03:45 +0000

    redeploy to railway

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,5 +3,6 @@ class Bookmark < ApplicationRecord
   belongs_to :list
 
   validates :movie_id, presence: true
+  # validates :movie_id, uniqueness: {scope: :list_id, message: "is already in the list"}
   # validates :comment, length: { minimum: 6 }
 end
